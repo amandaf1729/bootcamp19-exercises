be3edf14a8c95dbbcb04227e8c741204fa664f4f
amandaf1729 <amandaf1729@gmail.com>
2019-01-09 17:56:54 -0500

    3AM solutions

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + "/view/index.html"));
 });
+
+app.get("/puppies", (req, res) => {
+  res.sendFile(path.join(__dirname + "/view/puppies.html"));
+});
+
+app.get("/travel", (req, res) => {
+  res.sendFile(path.join(__dirname + "/view/travel.html"));
+});
+
+app.get("/memes", (req, res) => {
+  res.sendFile(path.join(__dirname + "/view/memes.html"));
+});
+
+//
 
 app.listen(3000);
 console.log("Running at port 3000");
